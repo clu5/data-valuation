74d57269fe98a7b477df0d14388c0fc745022a27
luchar <luchar>
2023-09-15 17:34:42 +0000

    change volume from cov to data

--- finetune.py
+++ finetune.py
@@ -1,22 +1,27 @@
 import copy
-from pathlib import Path
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+from pathlib import Path
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 import click
-from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.utils.data as data
 import torchvision
-from torchvision.transforms import transforms
 from torchvision import models
-import numpy as np
-import matplotlib.pyplot as plt; plt.style.use('bmh')
-import pandas as pd
-from sklearn import metrics
+from torchvision.transforms import transforms
+from tqdm import tqdm
+
+plt.style.use("bmh")
 from pathlib import Path
-from PIL import Image
+
+import pandas as pd
 import wilds
-from wilds.common.data_loaders import get_train_loader, get_eval_loader
+from PIL import Image
+from sklearn import metrics
+from wilds.common.data_loaders import get_eval_loader, get_train_loader
+
 import train
 
 
@@ -34,7 +39,7 @@ def get_q_hat(calibration_scores, labels, alpha=0.05):
 
     #  get quantile with small correction for finite sample sizes
     q_hat = torch.quantile(cum_scores, np.ceil((n + 1) * (1 - alpha)) / n)
-    clipped_q_hat = torch.minimum(q_hat, torch.tensor(1-1e-9))
+    clipped_q_hat = torch.minimum(q_hat, torch.tensor(1 - 1e-9))
 
     return clipped_q_hat
 
@@ -42,7 +47,7 @@ def get_q_hat(calibration_scores, labels, alpha=0.05):
 def make_prediction_set(scores, q_hat):
     if not isinstance(scores, torch.Tensor):
         scores = torch.tensor(scores)
-    assert q_hat < 1, 'q_hat must be below 1'
+    assert q_hat < 1, "q_hat must be below 1"
 
     n = scores.shape[0]
 
@@ -51,22 +56,24 @@ def make_prediction_set(scores, q_hat):
     #  number of each confidence prediction set to acheive coverage
     set_sizes = (values.cumsum(1) > q_hat).int().argmax(dim=1)
 
-    confidence_sets = [indices[i][0:(set_sizes[i] + 1)] for i in range(n)]
+    confidence_sets = [indices[i][0 : (set_sizes[i] + 1)] for i in range(n)]
 
     return [x.tolist() for x in confidence_sets]
 
+
 get_sizes = lambda x: np.array([len(x_i) for x_i in x])
 # for param in model.parameters():
-    # param.requires_grad = False
+# param.requires_grad = False
+
 
 @click.command()
-@click.option('-cp', '--checkpoint_path', default='models/rxrx1-b3.pt')
-@click.option('-dr', '--data_root', default='/u/luchar/data/wilds/')
+@click.option("-cp", "--checkpoint_path", default="models/rxrx1-b3.pt")
+@click.option("-dr", "--data_root", default="/u/luchar/data/wilds/")
 # @click.option('-lp', '--linear-probing')
-@click.option('-d', '--debug', is_flag=True, default=False)
+@click.option("-d", "--debug", is_flag=True, default=False)
 def main(checkpoint_path, data_root, debug):
     checkpoint_path = Path(checkpoint_path)
-    _dataset, arch = checkpoint_path.stem.split('-')
+    _dataset, arch = checkpoint_path.stem.split("-")
     checkpoint = torch.load(checkpoint_path)
     dataset = wilds.get_dataset(
         dataset=_dataset,
@@ -74,31 +81,31 @@ def main(checkpoint_path, data_root, debug):
     )
     num_classes = dataset._n_classes
 
-    model = getattr(models, f'efficientnet_{arch}')()
-    model.classifier[1] = torch.nn.Linear(
-        model.classifier[1].in_features,
-        num_classes
-    )
-    model.load_state_dict(checkpoint['model'])
+    model = getattr(models, f"efficientnet_{arch}")()
+    model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, num_classes)
+    model.load_state_dict(checkpoint["model"])
     model = model.cuda()
 
-    compose_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-    ])
+    compose_transforms = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+        ]
+    )
 
-    id_train_data = dataset.get_subset('train', transform=compose_transforms)
-    id_test_data = dataset.get_subset('id_test', transform=compose_transforms)
-    od_val_data = dataset.get_subset('val', transform=compose_transforms)
-    od_test_data = dataset.get_subset('test', transform=compose_transforms)
+    id_train_data = dataset.get_subset("train", transform=compose_transforms)
+    id_test_data = dataset.get_subset("id_test", transform=compose_transforms)
+    od_val_data = dataset.get_subset("val", transform=compose_transforms)
+    od_test_data = dataset.get_subset("test", transform=compose_transforms)
 
     subset = lambda ds, indices: data.Subset(ds, indices)
-    loader = lambda ds: data.DataLoader(ds, batch_size=16, num_workers=8, pin_memory=True, shuffle=False)
+    loader = lambda ds: data.DataLoader(
+        ds, batch_size=16, num_workers=8, pin_memory=True, shuffle=False
+    )
 
-    cal_split = int(len(id_test_data)*0.1)
+    cal_split = int(len(id_test_data) * 0.1)
     id_cal_data = subset(id_test_data, range(cal_split))
     id_test_data = subset(id_test_data, range(cal_split, len(id_test_data)))
-
 
     if debug:
         N = 1000
@@ -112,9 +119,9 @@ def main(checkpoint_path, data_root, debug):
     od_val_scores, od_val_labels = train.get_scores(model, loader(od_val_data))
     od_test_scores, od_test_labels = train.get_scores(model, loader(od_test_data))
 
-    print('original id accuracy')
+    print("original id accuracy")
     train.accuracy(id_test_scores, id_test_labels)
-    print('original od accuracy')
+    print("original od accuracy")
     train.accuracy(od_test_scores, od_test_labels)
 
     alpha = 0.10
@@ -126,7 +133,7 @@ def main(checkpoint_path, data_root, debug):
     od_val_size = get_sizes(od_val_pset)
     od_test_size = get_sizes(od_test_pset)
 
-    #budget_per_round = int(0.3 * len(od_val_data))
+    # budget_per_round = int(0.3 * len(od_val_data))
 
     q1 = np.quantile(od_test_size, 0.25)
     q2 = np.quantile(od_test_size, 0.5)
@@ -137,7 +144,9 @@ def main(checkpoint_path, data_root, debug):
 
     criterion = torch.nn.CrossEntropyLoss()
     lr = 1e-4
-    fit_kwargs = dict(criterion=criterion, epochs=20 if not debug else 2, debug=debug, patience=5)
+    fit_kwargs = dict(
+        criterion=criterion, epochs=20 if not debug else 2, debug=debug, patience=5
+    )
 
     bought_index = [i for i, size in enumerate(od_test_size) if size < q1]
     random_index = np.random.randint(0, len(od_test_size), size=len(bought_index))
@@ -147,19 +156,39 @@ def main(checkpoint_path, data_root, debug):
     model_random = copy.deepcopy(model).cuda()
     opt_bought = torch.optim.Adam(model_bought.parameters(), lr=lr)
     opt_random = torch.optim.Adam(model_random.parameters(), lr=lr)
-    model_bought = train.fit(model_bought, loader(bought_data), val_loader=loader(id_test_data), optimizer=opt_bought, **fit_kwargs)
-    model_random = train.fit(model_random, loader(random_data), val_loader=loader(id_test_data), optimizer=opt_random, **fit_kwargs)
-    id_bought_scores, id_bought_labels = train.get_scores(model_bought, loader(id_test_data))
-    id_random_scores, id_random_labels = train.get_scores(model_random, loader(id_test_data))
-    print('\nq1 finetuned id accuracy (bought) = ', end=' ')
+    model_bought = train.fit(
+        model_bought,
+        loader(bought_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_bought,
+        **fit_kwargs,
+    )
+    model_random = train.fit(
+        model_random,
+        loader(random_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_random,
+        **fit_kwargs,
+    )
+    id_bought_scores, id_bought_labels = train.get_scores(
+        model_bought, loader(id_test_data)
+    )
+    id_random_scores, id_random_labels = train.get_scores(
+        model_random, loader(id_test_data)
+    )
+    print("\nq1 finetuned id accuracy (bought) = ", end=" ")
     train.accuracy(id_bought_scores, id_bought_labels)
-    print('\nq1 finetuned id accuracy (random) = ', end=' ')
+    print("\nq1 finetuned id accuracy (random) = ", end=" ")
     train.accuracy(id_random_scores, id_random_labels)
-    od_bought_scores, od_bought_labels = train.get_scores(model_bought, loader(od_test_data))
-    od_random_scores, od_random_labels = train.get_scores(model_random, loader(od_test_data))
-    print('\nq1 finetuned od accuracy (bought) = ', end=' ')
+    od_bought_scores, od_bought_labels = train.get_scores(
+        model_bought, loader(od_test_data)
+    )
+    od_random_scores, od_random_labels = train.get_scores(
+        model_random, loader(od_test_data)
+    )
+    print("\nq1 finetuned od accuracy (bought) = ", end=" ")
     train.accuracy(od_bought_scores, od_bought_labels)
-    print('\nq1 finetuned od accuracy (random) = ', end=' ')
+    print("\nq1 finetuned od accuracy (random) = ", end=" ")
     train.accuracy(od_random_scores, od_random_labels)
 
     del model_bought, model_random, opt_bought, opt_random
@@ -173,19 +202,39 @@ def main(checkpoint_path, data_root, debug):
     model_random = copy.deepcopy(model).cuda()
     opt_bought = torch.optim.Adam(model_bought.parameters(), lr=lr)
     opt_random = torch.optim.Adam(model_random.parameters(), lr=lr)
-    model_bought = train.fit(model_bought, loader(bought_data), val_loader=loader(id_test_data), optimizer=opt_bought, **fit_kwargs)
-    model_random = train.fit(model_random, loader(random_data), val_loader=loader(id_test_data), optimizer=opt_random, **fit_kwargs)
-    id_bought_scores, id_bought_labels = train.get_scores(model_bought, loader(id_test_data))
-    id_random_scores, id_random_labels = train.get_scores(model_random, loader(id_test_data))
-    print('\nq2 finetuned id accuracy (bought) = ', end=' ')
+    model_bought = train.fit(
+        model_bought,
+        loader(bought_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_bought,
+        **fit_kwargs,
+    )
+    model_random = train.fit(
+        model_random,
+        loader(random_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_random,
+        **fit_kwargs,
+    )
+    id_bought_scores, id_bought_labels = train.get_scores(
+        model_bought, loader(id_test_data)
+    )
+    id_random_scores, id_random_labels = train.get_scores(
+        model_random, loader(id_test_data)
+    )
+    print("\nq2 finetuned id accuracy (bought) = ", end=" ")
     train.accuracy(id_bought_scores, id_bought_labels)
-    print('\nq2 finetuned id accuracy (random) = ', end=' ')
+    print("\nq2 finetuned id accuracy (random) = ", end=" ")
     train.accuracy(id_random_scores, id_random_labels)
-    od_bought_scores, od_bought_labels = train.get_scores(model_bought, loader(od_test_data))
-    od_random_scores, od_random_labels = train.get_scores(model_random, loader(od_test_data))
-    print('\nq2 finetuned od accuracy (bought) = ', end=' ')
+    od_bought_scores, od_bought_labels = train.get_scores(
+        model_bought, loader(od_test_data)
+    )
+    od_random_scores, od_random_labels = train.get_scores(
+        model_random, loader(od_test_data)
+    )
+    print("\nq2 finetuned od accuracy (bought) = ", end=" ")
     train.accuracy(od_bought_scores, od_bought_labels)
-    print('\nq2 finetuned od accuracy (random) = ', end=' ')
+    print("\nq2 finetuned od accuracy (random) = ", end=" ")
     train.accuracy(od_random_scores, od_random_labels)
 
     del model_bought, model_random, opt_bought, opt_random
@@ -199,25 +248,45 @@ def main(checkpoint_path, data_root, debug):
     model_random = copy.deepcopy(model).cuda()
     opt_bought = torch.optim.Adam(model_bought.parameters(), lr=lr)
     opt_random = torch.optim.Adam(model_random.parameters(), lr=lr)
-    model_bought = train.fit(model_bought, loader(bought_data), val_loader=loader(id_test_data), optimizer=opt_bought, **fit_kwargs)
-    model_random = train.fit(model_random, loader(random_data), val_loader=loader(id_test_data), optimizer=opt_random, **fit_kwargs)
-    id_bought_scores, id_bought_labels = train.get_scores(model_bought, loader(id_test_data))
-    id_random_scores, id_random_labels = train.get_scores(model_random, loader(id_test_data))
-    print('\nq3 finetuned id accuracy (bought) = ', end=' ')
+    model_bought = train.fit(
+        model_bought,
+        loader(bought_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_bought,
+        **fit_kwargs,
+    )
+    model_random = train.fit(
+        model_random,
+        loader(random_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_random,
+        **fit_kwargs,
+    )
+    id_bought_scores, id_bought_labels = train.get_scores(
+        model_bought, loader(id_test_data)
+    )
+    id_random_scores, id_random_labels = train.get_scores(
+        model_random, loader(id_test_data)
+    )
+    print("\nq3 finetuned id accuracy (bought) = ", end=" ")
     train.accuracy(id_bought_scores, id_bought_labels)
-    print('\nq3 finetuned id accuracy (random) = ', end=' ')
+    print("\nq3 finetuned id accuracy (random) = ", end=" ")
     train.accuracy(id_random_scores, id_random_labels)
-    od_bought_scores, od_bought_labels = train.get_scores(model_bought, loader(od_test_data))
-    od_random_scores, od_random_labels = train.get_scores(model_random, loader(od_test_data))
-    print('\nq3 finetuned od accuracy (bought) = ', end=' ')
+    od_bought_scores, od_bought_labels = train.get_scores(
+        model_bought, loader(od_test_data)
+    )
+    od_random_scores, od_random_labels = train.get_scores(
+        model_random, loader(od_test_data)
+    )
+    print("\nq3 finetuned od accuracy (bought) = ", end=" ")
     train.accuracy(od_bought_scores, od_bought_labels)
-    print('\nq3 finetuned od accuracy (random) = ', end=' ')
+    print("\nq3 finetuned od accuracy (random) = ", end=" ")
     train.accuracy(od_random_scores, od_random_labels)
 
     del model_bought, model_random, opt_bought, opt_random
     torch.cuda.empty_cache()
 
-    bought_index = [i for i, size in enumerate(od_test_size) if q3 < size ]
+    bought_index = [i for i, size in enumerate(od_test_size) if q3 < size]
     random_index = np.random.randint(0, len(od_test_size), size=len(bought_index))
     bought_data = subset(od_test_data, bought_index)
     random_data = subset(od_test_data, random_index)
@@ -225,20 +294,41 @@ def main(checkpoint_path, data_root, debug):
     model_random = copy.deepcopy(model).cuda()
     opt_bought = torch.optim.Adam(model_bought.parameters(), lr=lr)
     opt_random = torch.optim.Adam(model_random.parameters(), lr=lr)
-    model_bought = train.fit(model_bought, loader(bought_data), val_loader=loader(id_test_data), optimizer=opt_bought, **fit_kwargs)
-    model_random = train.fit(model_random, loader(random_data), val_loader=loader(id_test_data), optimizer=opt_random, **fit_kwargs)
-    id_bought_scores, id_bought_labels = train.get_scores(model_bought, loader(id_test_data))
-    id_random_scores, id_random_labels = train.get_scores(model_random, loader(id_test_data))
-    print('\nq4 finetuned id accuracy (bought) = ', end=' ')
+    model_bought = train.fit(
+        model_bought,
+        loader(bought_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_bought,
+        **fit_kwargs,
+    )
+    model_random = train.fit(
+        model_random,
+        loader(random_data),
+        val_loader=loader(id_test_data),
+        optimizer=opt_random,
+        **fit_kwargs,
+    )
+    id_bought_scores, id_bought_labels = train.get_scores(
+        model_bought, loader(id_test_data)
+    )
+    id_random_scores, id_random_labels = train.get_scores(
+        model_random, loader(id_test_data)
+    )
+    print("\nq4 finetuned id accuracy (bought) = ", end=" ")
     train.accuracy(id_bought_scores, id_bought_labels)
-    print('\nq4 finetuned id accuracy (random) = ', end=' ')
+    print("\nq4 finetuned id accuracy (random) = ", end=" ")
     train.accuracy(id_random_scores, id_random_labels)
-    od_bought_scores, od_bought_labels = train.get_scores(model_bought, loader(od_test_data))
-    od_random_scores, od_random_labels = train.get_scores(model_random, loader(od_test_data))
-    print('\nq4 finetuned od accuracy (bought) = ', end=' ')
+    od_bought_scores, od_bought_labels = train.get_scores(
+        model_bought, loader(od_test_data)
+    )
+    od_random_scores, od_random_labels = train.get_scores(
+        model_random, loader(od_test_data)
+    )
+    print("\nq4 finetuned od accuracy (bought) = ", end=" ")
     train.accuracy(od_bought_scores, od_bought_labels)
-    print('\nq4 finetuned od accuracy (random) = ', end=' ')
+    print("\nq4 finetuned od accuracy (random) = ", end=" ")
     train.accuracy(od_random_scores, od_random_labels)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
